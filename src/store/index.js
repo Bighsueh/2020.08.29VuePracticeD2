d90c006e110a33001cfc41d19b9fcd17239d23b3
Hsueh <40841121@gm.nfu.edu.tw>
2020-08-29 16:06:25 +0800

    add UserList 增加欄位功能

--- src/store/index.js
+++ src/store/index.js
@@ -4,22 +4,35 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state:{
-        users:[],
+    state: {
+        users: [],
     },
-    actions:{
+    actions: {
         GET_USER_LIST(context) {
             fetch("http://localhost:3000/user").then((res) => {
                 return res.json()
             }).then((data) => [
-                context.commit("SET_USER_LIST",data)
+                context.commit("SET_USER_LIST", data)
             ])
+        },
+        ADD_USER(context,data){
+            fetch("http://localhost:3000/user",{
+                body: JSON.stringify(data),
+                method: "POST",})
+                .then((res)=>{
+                    return res.json()
+                }).then((data)=>{
+                    console.log(data)
+            })
         }
     },
-    mutations:{
-        SET_USER_LIST(state,data){
+    mutations: {
+        SET_USER_LIST(state, data) {
             state.users = data
-}
+        },
+        ADD_USER(state, data) {
+            state.users.push(data);
+        }
     }
 
 })
--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -4,15 +4,15 @@
     <router-link v-for="item in users" :key="item.id" :to="'/user/'+item.id">{{item.name}}<br></router-link>
 
     <p>
-      Name: <input type="text">
+      Name: <input type="text" v-model.trim="name">
     </p>
     <p>
       Gender:
-      <input type="radio" value="1">男
-      <input type="radio" value="2">女
+      <input type="radio" value="1" v-model.number="gender">男
+      <input type="radio" value="2" v-model.number="gender">女
     </p>
     <p>
-      <button>送出</button>
+      <button @click="clickHandler">送出</button>
     </p>
 
   </div>
@@ -21,7 +21,12 @@
 
 <script>
 export default {
-  name: "UserList",
+  data(){
+    return{
+      name:"",
+      gender:0,
+    }
+  },
   created() {
     this.$store.dispatch("GET_USER_LIST")
   },
@@ -29,8 +34,17 @@ export default {
     users(){
       return this.$store.state.users
     }
+  },
+  methods:{
+    clickHandler(){
+      this.$store.dispatch("ADD_USER",{
+        name:this.name,
+        gender:this.gender
+      })
+      this.name = ""
+      this.gender = 0
+    }
   }
-
 }
 
 </script>
