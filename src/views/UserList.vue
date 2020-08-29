8da4c4f0558e2eb718c9a1f80c1bbe55bbc476ec
Hsueh <40841121@gm.nfu.edu.tw>
2020-08-29 14:58:08 +0800

    將資料放入compute , 資料套入前端

--- src/views/UserList.vue
+++ src/views/UserList.vue
@@ -1,17 +1,23 @@
 <template>
-  <div>This is UserList.</div>
+  <div>
+    <p>This is UserList.</p>
+    <router-link v-for="item in users" :key="item.id" :to="'/user/'+item.id">{{item.name}}<br></router-link>
+
+
+  </div>
+
 </template>
 
 <script>
 export default {
   name: "UserList",
-  data() {
-    return {
-      users: []
-    }
-  },
   created() {
     this.$store.dispatch("GET_USER_LIST")
+  },
+  computed:{
+    users(){
+      return this.$store.state.users
+    }
   }
 
 }
