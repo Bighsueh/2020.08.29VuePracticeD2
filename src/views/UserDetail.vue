064f0058bb52d38c9e17126bd48a2eaf6d7ed3e0
Hsueh <40841121@gm.nfu.edu.tw>
2020-08-29 17:07:13 +0800

    add beforeCreate路徑檢查功能

--- src/views/UserDetail.vue
+++ src/views/UserDetail.vue
@@ -4,9 +4,18 @@
 
 <script>
 export default {
-  beforeRouterEnter(to, from, next) {
-    console.log('beforeRouterEnter', to, from);
-    next()
-  }
+  // beforeRouterEnter(to, from, next) {
+  //   console.log('beforeRouterEnter', to, from);
+  //   next()
+  // }
+  beforeCreate() {
+    // console.log("beforeCreate",this.$store.state.users) ->輸出數值為num
+    // console.log("beforeCreate",this.$route.params.id)   ->輸出數值為string
+    let user = this.$store.state.users.find((user)=>{
+      return user.id="" === this.$route.params.id
+    })
+    console.log(user)
+  },
+
 }
 </script>
